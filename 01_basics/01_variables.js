e651d7363c36b4f3dd122cafaf7c0c83a9b402c3
FAIRLYFIRE <58458290+FAIRLYFIRE@users.noreply.github.com>
2024-02-05 07:09:32 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 76543
+let accountEmail = "jhgf@googe.com"
+var accountPassword = "54678"
+accountCity = "goa"
+
+// accountId = 76542
+accountEmail ="abc@yahoo.com"
+accountPassword = "gbjn9878"
+accountCity = "delhi"
+
+console.log(accountId);
+console.table([accountId, accountEmail,accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+"use strict" ; //treat all JS code as newer version
+
+// alert(6+2) we are nodejs , not browser
+
+/*
+Datatypes:
+1. number => 2 to the power 53
+*/
