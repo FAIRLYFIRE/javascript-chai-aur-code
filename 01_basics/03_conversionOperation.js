e64f05691e2773584d243dcf83fb3538dbd8e5d1
FAIRLYFIRE <58458290+FAIRLYFIRE@users.noreply.github.com>
2024-02-05 14:03:30 +0000

    questions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "somya"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+// "33" => 33
+// "83hj" => NaN
+// true => 1; false => 0
+
+let isLoggedin = 0
+let booleanIsLoggedin = Boolean(isLoggedin)
+
+console.log(booleanIsLoggedin);
+
+let k = null;
+let n=undefined;
+console.log(2+"2");
+console.log("2"+2);
+console.log(2-"2");
+console.log("2"-2);
