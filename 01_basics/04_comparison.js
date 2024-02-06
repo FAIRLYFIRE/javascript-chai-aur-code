b37d777d16b9dad322d660f0a45a39a22e7d6f4e
FAIRLYFIRE <58458290+FAIRLYFIRE@users.noreply.github.com>
2024-02-06 13:21:24 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+
+// console.log(2>"1");
+// console.log(2 > "01");
+
+// == does not convert datatype
+// console.log(null == 0);
+
+//converts null to number
+// console.log(null >= 0);
+// console.log(null <= 0);
+// console.log(undefined <= 0);
+// console.log(undefined == 0);
+
+// strict check ===
+// checks values and datatypes
+console.log("2" === 2);
+
+
+
+
+
