aecffd231ca536db763c6019e0def9b3cf27e6fc
FAIRLYFIRE <58458290+FAIRLYFIRE@users.noreply.github.com>
2024-02-06 12:48:26 +0000

    datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,23 +1,50 @@
 let score = "somya"
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
 
 // "33" => 33
 // "83hj" => NaN
 // true => 1; false => 0
 
-let isLoggedin = 0
+let isLoggedin = " "
 let booleanIsLoggedin = Boolean(isLoggedin)
 
-console.log(booleanIsLoggedin);
+// console.log(booleanIsLoggedin);
 
+// 1 => true
+// 0 => false
+// "" => false
+// "value" => true
 let k = null;
 let n=undefined;
-console.log(2+"2");
-console.log("2"+2);
-console.log(2-"2");
-console.log("2"-2);
+
+
+// *****************Operations**************************
+
+let value = 7
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+"2");
+// console.log("2"+2);
+// console.log(2-"2");
+// console.log("2"-2); 
+// console.log(2 +1 + "4");
+// console.log(2 + "2" + 4);
+// console.log("7" + 3 + 2);
+
+// console.log(3+4*5%3);
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+ 2
+// console.log(num1, num2, num3);
+
+let gameCounter = 100
+console.log(++gameCounter)
+console.log(gameCounter);
