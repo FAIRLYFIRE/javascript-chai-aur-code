ffe8a9bfcbea5b31f2047a44c8f802934ee361af
FAIRLYFIRE <58458290+FAIRLYFIRE@users.noreply.github.com>
2024-02-26 12:51:42 +0000

    Maths

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,15 +1,32 @@
 const score = 400
-console.log(score);
+// console.log(score);
 
 const balance = new Number(799)
-console.log(balance);
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
 
 const otherNumber = 123.8606
-console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(3));
 
 const hundreds = 1000000000
-console.log(hundreds.toLocaleString('en-in'));
+// console.log(hundreds.toLocaleString('en-in'));
 
 /////////***************Maths **************/////////////
+
+// console.log(Math.abs(-7));
+// console.log(Math.round(6.98));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(7.8));
+// console.log(Math.min(7,9,-3,0,4));
+// console.log(Math.max(-7,9,5,2,8));`
+
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
+console.log(Math.floor((Math.random()*10)+1));
+
+// Generate random numbers between a min and max value
+const min = 10
+const max = 20
+console.log((Math.floor(Math.random() * (max-min + 1)))+ min);
+
